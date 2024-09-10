5418e7e2ee25f1acc818a25a6e6379ae2d4ac8f9
Mukesh05121993 <Mukesh.kristal@gmail.com>
2024-09-10 11:29:53 +0530

    Update Trail1.java

--- src/main/java/test123/Trail1.java
+++ src/main/java/test123/Trail1.java
@@ -4,6 +4,7 @@ public class Trail1 {
 
 	public static void main(String[] args) {
 		int a = 10;
+				int b = 10;
 		// TODO Auto-generated method stub
 System.out.print("Hai Welcome");
 System.out.print("Hai Welcome");
