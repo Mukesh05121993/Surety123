b179b0944819f5351ce88a120df0a7334331413f
Pushpalatha R <latha121096@gmail.com>
2024-09-10 11:02:00 +0530

    updated the Trail1 file

--- src/main/java/test123/Trail1.java
+++ src/main/java/test123/Trail1.java
@@ -5,6 +5,8 @@ public class Trail1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.print("Hai Welcome");
+System.out.print("Hai Welcome");
+System.out.print("Hai Welcome");
 	}
 
 }
